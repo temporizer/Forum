e5585fe96eac2cef9d9273ba34534f66c4207b72
unknown <jacobsifuen@hotmail.com>
2012-12-26 01:33:57 -0500

    forum category listing works

--- index.php
+++ index.php
@@ -1,51 +1,33 @@
 <?php
 	require(dirname(__FILE__) . '/backend/header.php');
 ?>
-			<div class="nine columns">
-				<h5 class="category">Main</h5>
-				<hr />
-				<div class="row">
-					<div class="one column mobile-one"><img src="http://placehold.it/50x50&text=[img]" /></div>
-					<div class="eleven columns"><strong>News and Annoucements</strong> <p class="forum_desc">Important news will be posted here.</p></div>
-				</div>
-				<hr />
-				<div class="row">
-					<div class="one column mobile-one"><img src="http://placehold.it/50x50&text=[img]" /></div>
-					<div class="eleven columns"><strong>Updates</strong> <p class="forum_desc">We sometimes have updates to our website. We post logs here.</p></div>
-				</div>
-				
-				<br /><br />	
-				
-				<h5 class="category">General</h5>
-				<hr />
-				<div class="row">
-					<div class="one column mobile-one"><img src="http://placehold.it/50x50&text=[img]" /></div>
-					<div class="eleven columns"><strong>Introductions</strong> <p class="forum_desc">Introduce yourself to the community.</p></div>
-				</div>
-				<hr />
-				<div class="row">
-					<div class="one column mobile-one"><img src="http://placehold.it/50x50&text=[img]" /></div>
-					<div class="eleven columns"><strong>Discussions</strong> <p class="forum_desc">Discuss whatever here. Off-topic area. Post count isn't affected.</p></div>
-				</div>
-				<hr />
-				<div class="row">
-					<div class="one column mobile-one"><img src="http://placehold.it/50x50&text=[img]" /></div>
-					<div class="eleven columns"><strong>Peer Review</strong> <p class="forum_desc">Ask us to review your website!</p></div>
-				</div>
-				
-				<br /><br />
-				
-				<h5 class="category">Main</h5>
-				<hr />
-				<div class="row">
-					<div class="one column mobile-one"><img src="http://placehold.it/50x50&text=[img]" /></div>
-					<div class="eleven columns"><strong>News and Annoucements</strong> <p class="forum_desc">Important news will be posted here.</p></div>
-				</div>
-				<hr />
-				<div class="row">
-					<div class="one column mobile-one"><img src="http://placehold.it/50x50&text=[img]" /></div>
-					<div class="eleven columns"><strong>Updates</strong> <p class="forum_desc">We sometimes have updates to our website. We post logs here.</p></div>
-				</div>
+			<div class="nine columns">				
+				<?php
+					//let's start getting forum categories
+					$cats = $User->query("SELECT id, name, description, parent, placement FROM categories ORDER BY placement ASC, id ASC");
+					
+					$i = 0;
+					foreach($cats AS $row)
+					{
+						if($row["parent"] == "yes")
+						{
+							echo ($i > 0) ? '<br />' : '';
+							echo '<h5 class="category">' . $row["name"] . '</h5>';
+							echo '<hr />';
+						}
+							else
+						{
+							echo '<div class="row">';
+							echo '<div class="one column mobile-one"><img src="http://placehold.it/50x50&text=[img]" /></div>';
+							echo '<div class="eleven columns"><strong>' . $row["name"] . '</strong>';
+							echo '<p class="forum_desc">' . $row["description"] . '</p></div>';
+							echo '</div>';
+							echo '<hr />';
+						}
+						
+						$i++;
+					}
+				?>
 			</div>
 <?php
 	require(dirname(__FILE__) . '/backend/footer.php');
